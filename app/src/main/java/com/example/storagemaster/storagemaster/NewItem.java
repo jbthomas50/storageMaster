b9cd32bc61a780644c0b1b0cead2255e12e3faa7
tho14063 <35983776+tho14063@users.noreply.github.com>
2018-02-26 15:06:33 -0700

    adding button

---
+++ app/src/main/java/com/example/storagemaster/storagemaster/NewItem.java
@@ -0,0 +1,13 @@
+package com.example.storagemaster.storagemaster;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewItem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_item);
+    }
+}
