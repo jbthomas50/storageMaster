749cc78b001f4bed514687e2d7a6c285acea2762
Alex Charles <alexjob44@gmail.com>
2018-02-27 10:38:12 -0700

    Added incomplete listview and changed add button icon

--- app/src/main/java/com/example/storagemaster/storagemaster/MainActivity.java
+++ app/src/main/java/com/example/storagemaster/storagemaster/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.storagemaster.storagemaster;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,6 +16,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -98,4 +108,39 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    private class MyListAdapter extends ArrayAdapter<String> {
+        private int layout;
+
+        public MyListAdapter(@NonNull Context context, int resource, @NonNull List<String> objects) {
+            super(context, resource, objects);
+            layout = resource;
+        }
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        ViewHolder mainViewHolder = null;
+        if(convertView == null){
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(layout, parent, false);
+            ViewHolder viewHolder = new ViewHolder();
+            viewHolder.name = (TextView) convertView.findViewById(R.id.itemname);
+            viewHolder.subtractB = (Button) convertView.findViewById(R.id.subtractbutton);
+            viewHolder.qty = (TextView) convertView.findViewById(R.id.quantity);
+            viewHolder.addB = (Button) convertView.findViewById(R.id.addbutton);
+            convertView.setTag(viewHolder);
+        }
+        else {
+            mainViewHolder = (ViewHolder) convertView.getTag();
+            mainViewHolder.name.setText(getItem(position));
+        }
+        return convertView;
+    }
+}
+
+public class ViewHolder {
+    TextView name;
+    Button subtractB;
+    TextView qty;
+    Button addB;
+}
 }
