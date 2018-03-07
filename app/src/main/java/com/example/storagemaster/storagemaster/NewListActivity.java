496eb98d3cdf9f112b1467cc327afac883c0b221
Henry Burden <bur15054@byui.edu>
2018-03-07 13:48:05 -0700

    added Item constructors

--- app/src/main/java/com/example/storagemaster/storagemaster/NewListActivity.java
+++ app/src/main/java/com/example/storagemaster/storagemaster/NewListActivity.java
@@ -1,8 +1,11 @@
 package com.example.storagemaster.storagemaster;
 
+import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.Menu;
+import android.widget.Toast;
 
 public class NewListActivity extends AppCompatActivity {
 
@@ -18,6 +21,18 @@ public class NewListActivity extends AppCompatActivity {
         int height = dm.heightPixels;
 
         getWindow().setLayout((int)(width*.8), (int)(height*.3));
+
+        Category newCategory = new Category();
+        newCategory.setCategoryName("newList " + MainActivity.inventory.size());
+        MainActivity.inventory.add(newCategory);
+
+        //Henry Function for adding a ListName/string to Nav Drawer
+        Menu menu = MainActivity.navigationView.getMenu(); //access to the nav drawer menu
+        MainActivity.addNavDrawerItems(menu);
+
+        //System.out.println("Inventory Size: " + MainActivity.inventory.size());
+
+
     }
 
 }
